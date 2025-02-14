c1ef84bb25d44df0f2b1f29000ea89bb5259bf73
Xavi24333 <javieralvizures2006@gmail.com>
2025-02-13 22:21:34 -0600

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -38,5 +38,81 @@ public class Sorts {
             return merged;
         }
     }
-//aaa
+public static class SelectionSort implements SortAlgorithm {
+        @Override
+        public int[] sort(int[] arr) {
+            int[] sortedArr = arr.clone();
+            int n = sortedArr.length;
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++) {
+                    if (sortedArr[j] < sortedArr[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+                int temp = sortedArr[minIndex];
+                sortedArr[minIndex] = sortedArr[i];
+                sortedArr[i] = temp;
+            }
+            return sortedArr;
+        }
+    }
+
+    public static class QuickSort implements SortAlgorithm {
+        @Override
+        public int[] sort(int[] arr) {
+            int[] sortedArr = arr.clone();
+            quickSortHelper(sortedArr, 0, sortedArr.length - 1);
+            return sortedArr;
+        }
+
+        private void quickSortHelper(int[] arr, int low, int high) {
+            if (low < high) {
+                int pi = partition(arr, low, high);
+                quickSortHelper(arr, low, pi - 1);
+                quickSortHelper(arr, pi + 1, high);
+            }
+        }
+
+        private int partition(int[] arr, int low, int high) {
+            int pivot = arr[high];
+            int i = low - 1;
+            for (int j = low; j < high; j++) {
+                if (arr[j] < pivot) {
+                    i++;
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            int temp = arr[i + 1];
+            arr[i + 1] = arr[high];
+            arr[high] = temp;
+            return i + 1;
+        }
+    }
+
+    public static class RadixSort implements SortAlgorithm {
+        @Override
+        public int[] sort(int[] arr) {
+            int[] sortedArr = arr.clone();
+            int max = Arrays.stream(sortedArr).max().orElse(0);
+            for (int exp = 1; max / exp > 0; exp *= 10) {
+                int[] output = new int[sortedArr.length];
+                int[] count = new int[10];
+                for (int num : sortedArr) {
+                    count[(num / exp) % 10]++;
+                }
+                for (int i = 1; i < 10; i++) {
+                    count[i] += count[i - 1];
+                }
+                for (int i = sortedArr.length - 1; i >= 0; i--) {
+                    output[count[(sortedArr[i] / exp) % 10] - 1] = sortedArr[i];
+                    count[(sortedArr[i] / exp) % 10]--;
+                }
+                sortedArr = output.clone();
+            }
+            return sortedArr;
+        }
+    }
 }
